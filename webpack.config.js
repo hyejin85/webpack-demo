f3cde97ccbae2afa2d58166ef05d9623254b9074
hyejin85.lee <hyejin85.lee@samsung.com>
2020-07-23 16:20:18 +0900

    First commit for webpack project
    - just made the frame of webpack project
    
    https://webpack.js.org/guides/getting-started/

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+import path from 'path';
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
